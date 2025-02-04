20e04c3f79b8ac771dac2b99321cbb359610bdc7
45Whynot54 <90743492+45Whynot54@users.noreply.github.com>
2025-02-04 08:38:42 +0300

    lab 1.5 убрана возможность поворота экрана только дизайн

--- app/src/main/java/com/example/labs/ui/MainActivity.kt
+++ app/src/main/java/com/example/labs/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.labs.ui
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
@@ -12,6 +13,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
     }
 }
