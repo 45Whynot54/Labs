84affd4a6198fe168b17db9aaa7aca8c4751b74d
45Whynot54 <90743492+45Whynot54@users.noreply.github.com>
2025-02-15 01:47:44 +0300

    main только ночной режим TODO: Обьяснения для лаб работ (1 и 2)

--- app/src/main/java/com/example/labs/ui/MainActivity.kt
+++ app/src/main/java/com/example/labs/ui/MainActivity.kt
@@ -3,12 +3,18 @@ package com.example.labs.ui
 import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.labs.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Only night mode
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+
+        //Only portrait orientation
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
 
     }
