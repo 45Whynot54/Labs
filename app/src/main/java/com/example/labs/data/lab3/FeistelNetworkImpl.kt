dbfcc8877bf26c09fa04d991042546c551dc8967
45Whynot54 <90743492+45Whynot54@users.noreply.github.com>
2025-03-18 16:53:39 +0300

    lab 3.8 как в методичке, но 2 блока

--- app/src/main/java/com/example/labs/data/lab3/FeistelNetworkImpl.kt
+++ app/src/main/java/com/example/labs/data/lab3/FeistelNetworkImpl.kt
@@ -1,6 +1,5 @@
 package com.example.labs.data.lab3
 
-import com.example.labs.data.lab3.FeistelNetworkKeyGenerator.key
 import com.example.labs.domain.lab3.FeistelNetwork
 
 object FeistelNetworkImpl : FeistelNetwork {
@@ -13,7 +12,7 @@ object FeistelNetworkImpl : FeistelNetwork {
         return encryptedMessage
     }
 
-        override fun feistelFunctionDecrypt(message: String, key: String, rounds: Int): String {
+    override fun feistelFunctionDecrypt(message: String, key: String, rounds: Int): String {
         var decryptedMessage = message
         repeat(rounds) { round ->
             decryptedMessage = reverseRoundFunction(decryptedMessage, key, rounds - round - 1)
@@ -23,23 +22,21 @@ object FeistelNetworkImpl : FeistelNetwork {
 
     private fun roundFunction(message: String, key: String, round: Int): String {
         val (left, right) = divideIntoBlocks(message)
-        val roundKey = key(key, round)
+        val roundKey = FeistelNetworkKeyGenerator.key(key, round)
         val newRight = xor(right, roundKey)
-        val newLeft = xor(left, newRight)
-        return newLeft + newRight
+        return newRight + left // Меняем местами левый и правый блоки
     }
 
     private fun reverseRoundFunction(message: String, key: String, round: Int): String {
         val (left, right) = divideIntoBlocks(message)
-        val roundKey = key(key, round)
-        val newLeft = xor(left, right)
-        val newRight = xor(right, roundKey)
-        return newLeft + newRight
+        val roundKey = FeistelNetworkKeyGenerator.key(key, round)
+        val newLeft = xor(left, roundKey)
+        return right + newLeft // Меняем местами левый и правый блоки
     }
 
     private fun divideIntoBlocks(message: String): Pair<String, String> {
         val mid = message.length / 2
-        val paddedMessage = if (message.length % 2 != 0) message + "" else message
+        val paddedMessage = if (message.length % 2 != 0) message + " " else message // Добавляем пробел, если длина нечетная
         return Pair(paddedMessage.substring(0, mid), paddedMessage.substring(mid))
     }
 
