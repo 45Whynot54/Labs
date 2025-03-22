2a183f7a663e40cc4343a5c1743c167a10caa221
45Whynot54 <90743492+45Whynot54@users.noreply.github.com>
2025-03-22 23:15:02 +0300

    lab 6.3 изменил в gradble номер сборки

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "1.4.10 release"
+        versionName = "1.6.3 release"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
