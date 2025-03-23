897092d795e6b64fde2ecae2ddc85a98ddb7a625
45Whynot54 <90743492+45Whynot54@users.noreply.github.com>
2025-03-24 00:03:25 +0300

    lab 7.5 все сделано

--- app/src/main/java/com/example/labs/ui/labFragments/lab7/Seven.kt
+++ app/src/main/java/com/example/labs/ui/labFragments/lab7/Seven.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.labs.R
@@ -19,13 +20,14 @@ import com.example.labs.ui.GenerateKey
 
 class Seven : Fragment() {
 
-    private var _binding: FragmentLab7Binding?= null
+    private var _binding: FragmentLab7Binding? = null
     private val binding: FragmentLab7Binding
         get() = _binding ?: throw RuntimeException("FragmentLab7Binding == null")
 
     private val viewModel: PasswordAndLoginVM by activityViewModels()
 
     private val generalFunctions = GeneralFunctionsImpl
+    private var isForgotMode: Boolean = false
 
     private val textWatcher = object : TextWatcher {
         override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
@@ -57,20 +59,35 @@ class Seven : Fragment() {
             btnRegistration.setOnClickListener {
                 findNavController().navigate(R.id.action_seven_to_registrationFragment)
             }
+            btnFoggot.setOnClickListener {
+                changeType()
+            }
         }
     }
 
     private fun entryInSystem() {
         val login = binding.login.text.toString().trim()
-        val password = binding.password.text.toString().trim()
-        if (login != viewModel.login.toString() || password != viewModel.password.toString()) {
-            generalFunctions.showShortToast(context, "Неправильный логин и/или пароль", 1000)
-            return
-        }
-        else{
-            showEntryInSystem()
+        val input = binding.password.text.toString().trim()
+
+        with(viewModel) {
+
+            if (isForgotMode) {
+                when {
+                    login != this.login -> generalFunctions.showShortToast(context, "Неверный логин", 1000)
+                    input.isEmpty() -> generalFunctions.showShortToast(context, "Введите ответ", 1000)
+                    input != answerOnQuestion -> generalFunctions.showShortToast(context, "Неверный ответ", 1000)
+                    else -> showEntryInSystem()
+                }
+            } else {
+                when {
+                    login != this.login -> generalFunctions.showShortToast(context, "Неверный логин", 1000)
+                    input != password -> generalFunctions.showShortToast(context, "Неверный пароль", 1000)
+                    else -> showEntryInSystem()
+                }
+            }
         }
     }
+
 
     private fun showEntryInSystem() {
         val bottomSheetOne = EntryInSystemFragment()
@@ -81,5 +98,22 @@ class Seven : Fragment() {
         val login = binding.login.text.toString()
         val password = binding.password.text.toString()
         binding.btnApply.isEnabled = login.isNotEmpty() && password.isNotEmpty()
+    }
+
+    private fun changeType() {
+        isForgotMode = !isForgotMode
+        with(binding) {
+            question.isVisible = isForgotMode
+            password.text?.clear()
+
+            if (isForgotMode) {
+                question.text = "${getString(R.string.your_question)} ${viewModel.question}"
+                btnFoggot.text = getString(R.string.remember_password)
+                password.hint = getString(R.string.answer)
+            } else {
+                btnFoggot.text = getString(R.string.foggot_password)
+                password.hint = getString(R.string.password)
+            }
+        }
     }
 }
